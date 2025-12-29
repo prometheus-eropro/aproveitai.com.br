fd7007e5983533159a9fddcedce75e125d80446a
prometheus-eropro <assistentes.prometheus@gmail.com>
2025-12-29 18:21:01 -0300

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,45 +1,16 @@
-const CACHE_NAME = "aproveitai-cache-v2";
-const FILES_TO_CACHE = [
-  "/index.html",
-  "/style.css",
-  "/manifest.json",
-  "/favicon.ico",
-  "/logo-aproveitai.png",
-  "/logo-prometheus.png",
-  "/painelcliente.html",
-  "/beneficios.html",
-  "/promocoes.html",
-  "/depoimentos.html",
-  "/cartao.html"
-];
-
-// Instala e guarda cache
-self.addEventListener("install", (evt) => {
-  evt.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
-  );
+self.addEventListener("install", () => {
   self.skipWaiting();
 });
 
-// Ativa e limpa caches antigos
-self.addEventListener("activate", (evt) => {
-  evt.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(
-        keyList.map((key) => {
-          if (key !== CACHE_NAME) return caches.delete(key);
-        })
-      );
-    })
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((key) => caches.delete(key)))
+    )
   );
   self.clients.claim();
 });
 
-// Intercepta requisições e tenta offline
-self.addEventListener("fetch", (evt) => {
-  evt.respondWith(
-    caches.match(evt.request).then((response) => {
-      return response || fetch(evt.request);
-    })
-  );
+self.addEventListener("fetch", (event) => {
+  event.respondWith(fetch(event.request));
 });
