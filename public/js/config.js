25e08818952ed6a112f3375f32cda2c357d59b51
prometheus-eropro <assistentes.prometheus@gmail.com>
2026-08-08 14:18:04 -0300

    Add files via upload

--- public/js/config.js
+++ public/js/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
 
-    versao: "2026.08.06-002",
+    versao: "2026.08.08-001",
     empresa: "PROMETHEUS EROPRO",
     plataforma: "PROMETHEUS HUB",
     sistema: "Cartão AproveitAI",
