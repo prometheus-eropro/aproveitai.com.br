29f8380c44f8b2d7c9dc3b6e9754b35c62a8e826
prometheus-eropro <assistentes.prometheus@gmail.com>
2026-08-18 18:25:58 -0300

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,19 @@
+// SERVICE WORKER EM MODO DESENVOLVIMENTO
+// Não faz cache, não força app, não trava atualização
+
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(fetch(event.request));
+});
